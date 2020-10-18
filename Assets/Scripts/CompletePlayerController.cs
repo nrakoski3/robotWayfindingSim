9d136357fa5e7c5cca550968286981284d24293f
William Syre <wsyre3@gatech.edu>
2020-10-18 17:25:13 -0400

    movement, tilemap generation

---
+++ Assets/Scripts/CompletePlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompletePlayerController : MonoBehaviour
+{
+
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("W pressed");
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/DungeonDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class DungeonDisplay : MonoBehaviour
+{
+    int[,] input_array;
+    public Tilemap tilemap;
+    public Tile base_tile;
+    public Tile floor_tile;
+    public Tile wall_tile;
+    public Tile floor2_tile;
+    void Start()
+    {
+        input_array = new int[100, 100];
+        init_input_array();
+        for (int i = 0; i < input_array.GetLength(0); i++)
+        {
+            for (int j = 0; j < input_array.GetLength(1); j++)
+            {
+                Debug.Log(input_array[i, j].ToString());
+                Vector3Int b = new Vector3Int(i, j, 0);
+                if (input_array[i, j] == 0)
+                {
+                    tilemap.SetTile(b, base_tile);
+                } else if (input_array[i, j] == 1)
+                {
+                    tilemap.SetTile(b, floor_tile);
+                }
+                else if (input_array[i, j] == 2)
+                {
+                    tilemap.SetTile(b, floor2_tile);
+                }
+                else if (input_array[i, j] == 3)
+                {
+                    tilemap.SetTile(b, wall_tile);
+                }
+            }
+        }
+        
+    }
+
+    void init_input_array()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            for (int j  = 0; j < 100; j++)
+            {
+                input_array[i, j] = (i + j) % 4;
+            }
+        }
+    }
+}
