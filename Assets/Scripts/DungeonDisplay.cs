9bcdbe5f52610d1f105647da98df108bc6e2d723
William Syre <wsyre3@gatech.edu>
2020-10-18 22:57:37 -0400

    got layered tilemap working

--- Assets/Scripts/DungeonDisplay.cs
+++ Assets/Scripts/DungeonDisplay.cs
@@ -10,11 +10,19 @@ using UnityEngine.Tilemaps;
 public class DungeonDisplay : MonoBehaviour
 {
     int[,] input_array;
-    public Tilemap tilemap;
+    public Tilemap floor_tilemap;
+    public Tilemap wall_tilemap;
+    public Tilemap decorations_tilemap;
+    public Tilemap collectibles_tilemap;
     public Tile base_tile;
     public Tile floor_tile;
     public Tile wall_tile;
-    public Tile floor2_tile;
+    public Tile floor_tile2;
+    public Tile wall_vertical;
+    public Tile wall_left_corner;
+    public Tile wall_right_corner;
+    public Tile wall_left;
+    public Tile wall_right;
     void Start()
     {
         input_array = new int[100, 100];
@@ -23,24 +31,45 @@ public class DungeonDisplay : MonoBehaviour
         {
             for (int j = 0; j < input_array.GetLength(1); j++)
             {
-                Debug.Log(input_array[i, j].ToString());
                 Vector3Int b = new Vector3Int(i, j, 0);
+                floor_tilemap.SetTile(b, base_tile);
                 if (input_array[i, j] == 0)
                 {
-                    tilemap.SetTile(b, base_tile);
-                } else if (input_array[i, j] == 1)
+                    floor_tilemap.SetTile(b, floor_tile);
+                }
+                else if (input_array[i, j] == 1)
                 {
-                    tilemap.SetTile(b, floor_tile);
+                    floor_tilemap.SetTile(b, floor_tile2);
                 }
                 else if (input_array[i, j] == 2)
                 {
-                    tilemap.SetTile(b, floor2_tile);
+                    floor_tilemap.SetTile(b, floor_tile);
                 }
                 else if (input_array[i, j] == 3)
                 {
-                    tilemap.SetTile(b, wall_tile);
+                    floor_tilemap.SetTile(b, floor_tile2);
                 }
-            }
+
+                if (i == 0)
+                {
+                    wall_tilemap.SetTile(b, wall_left);
+                }
+
+                if (i == input_array.GetLength(0) - 1)
+                {
+                    wall_tilemap.SetTile(b, wall_right);
+                }
+
+                if (j == 0)
+                {
+                    wall_tilemap.SetTile(b, wall_vertical);
+                }
+
+                if (j == input_array.GetLength(1) - 1)
+                {
+                    wall_tilemap.SetTile(b, wall_vertical);
+                }
+            }       
         }
         
     }
@@ -51,7 +80,7 @@ public class DungeonDisplay : MonoBehaviour
         {
             for (int j  = 0; j < 100; j++)
             {
-                input_array[i, j] = (i + j) % 4;
+                input_array[i, j] = (j) % 4;
             }
         }
     }
